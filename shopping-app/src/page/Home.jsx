5995dd433098cb5178e34816d29714428411925d
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-02 19:18:14 +0100

    mapped every data product and added to product cart componant as props

--- shopping-app/src/page/Home.jsx
+++ shopping-app/src/page/Home.jsx
@@ -1,8 +1,25 @@
+import { useContext, useEffect } from "react";
+import { ShoppingContext } from "../context/ShoppingContext";
+import ProductCard from "../componants/ProductCard";
+
 function Home() {
+  const { data, loading, error, fetchProducts } = useContext(ShoppingContext);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  if (!data) return null;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  if (error) return <h1>{error}</h1>;
+
   return (
     <>
-      <h1>Home page</h1>
-      <p>Welcome</p>
+      {data.products.map((product) => (
+        <ProductCard product={product} />
+      ))}
     </>
   );
 }
