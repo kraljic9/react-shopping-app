76b74cd21d7368ab3067ece3d17e308691904d01
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-02 17:27:22 +0100

    Added Routes to home and products page

--- shopping-app/src/App.jsx
+++ shopping-app/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./page/Home";
+import ProductPage from "./page/ProductPage";
 
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
