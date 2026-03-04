dc69e8ab1f67a254813f74681b889a20a5956d54
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-04 14:38:19 +0100

    Added dynamic routing to product pages

--- shopping-app/src/componants/ProductCard.jsx
+++ shopping-app/src/componants/ProductCard.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function ProductCard({ product }) {
+  const navigate = useNavigate();
+
+  function navigateProductPage() {
+    navigate(`/product/${product.id}`);
+  }
+
   return (
-    <div className="product-card-container">
+    <div className="product-card-container" onClick={navigateProductPage}>
       <img src={product.images[0]} alt="" className="product-card-img" />
       <p className="product-card-title">{product.title}</p>
       <p className="product-card-price">{product.price}$</p>
