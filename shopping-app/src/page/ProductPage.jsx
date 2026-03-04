1b194d98901e528a5b0928af657b3071548e374d
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-04 16:26:07 +0100

    Added content to products page

--- shopping-app/src/page/ProductPage.jsx
+++ shopping-app/src/page/ProductPage.jsx
@@ -40,15 +40,25 @@ function ProductPage() {
   return (
     <div className="product-page-container">
       <div className="product-page-images-container">
-        <div className="main-image"></div>
-        <div className="slecet-images"></div>
+        <div className="main-image-container">
+          <img src={data.images[0]} alt="" className="main-image" />
+        </div>
+        <div className="slecet-images">
+          {data.images.map((image) => {
+            return (
+              <div className="select-image-box" key={image}>
+                <img src={image} alt="" />
+              </div>
+            );
+          })}
+        </div>
       </div>
 
       <div className="product-page-information">
-        <p className="brand"></p>
-        <h1 className="product-page-name"></h1>
-        <p className="product-page-information"></p>
-        <p className="product-page-price"></p>
+        <p className="brand">{data.brand}</p>
+        <h1 className="product-page-name">{data.title}</h1>
+        <p className="product-page-information">{data.description}</p>
+        <p className="product-page-price">${data.price}</p>
 
         <div className="product-page-information-buttons">
           <div className="set-ammount-container">
@@ -56,10 +66,22 @@ function ProductPage() {
             <div className="prodcut-ammount">0</div>
             <button className="subtract-ammount">-</button>
           </div>
+
+          <button className="add-to-cart-btn">Add to cart</button>
         </div>
       </div>
 
-      <div className="product-page-comments"></div>
+      <div className="product-page-reviews">
+        {data.reviews.map((review) => {
+          return (
+            <div className="review-box" key={review.comment}>
+              <p className="reviewer-name">{review.reviewerName}</p>
+              <p className="review-rating">Rating: {review.rating}/5</p>
+              <p className="review-comment">{review.comment}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
