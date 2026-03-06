0fd66c0bbe0dd6dec3c757c8aa192cc16784c04d
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-06 12:43:00 +0100

    Added new jsx files CartPage and CartContainer

---
+++ shopping-app/src/componants/CartContainer.jsx
@@ -0,0 +1,9 @@
+function CartContainer() {
+  return (
+    <div className="cart-container">
+      <p>welcome</p>
+    </div>
+  );
+}
+
+export default CartContainer;
---
+++ shopping-app/src/page/CartPage.jsx
@@ -0,0 +1,11 @@
+function CartPage() {
+  return (
+    <>
+      <h1>Welcome to your cart</h1>
+
+      <p>Where are your items...</p>
+    </>
+  );
+}
+
+export default CartPage;
