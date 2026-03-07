1748ca6d90e5f37d3f78d9d56f7db13b719b33af
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-07 17:43:40 +0100

    Added items for layout, multiple containers for image, product information and select

--- shopping-app/src/page/CartPage.jsx
+++ shopping-app/src/page/CartPage.jsx
@@ -1,10 +1,71 @@
-function CartPage() {
-  return (
-    <>
-      <h1>Welcome to your cart</h1>
+import { useContext, useEffect } from "react";
+import { ShoppingContext } from "../context/ShoppingContext";
 
-      <p>Where are your items...</p>
-    </>
+function CartPage() {
+  const { shoppingCart, setShoppingCart } = useContext(ShoppingContext);
+
+  useEffect(() => {
+    localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+  }, [shoppingCart]);
+
+  console.log(shoppingCart);
+
+  return (
+    <div className="cart-page-container">
+      <div className="cart-page-product-container">
+        {shoppingCart.map((item) => (
+          <div className="cart-page-product-box" key={item.id}>
+            <div className="cart-page-product-img-container">
+              <img
+                src={item.images[0]}
+                alt=""
+                className="cart-page-product-img"
+              />
+            </div>
+
+            <div className="cart-page-proudct-info">
+              <p className="cart-page-product-name">{item.title}</p>
+              <p className="cart-page-product-brand">{item.brand}</p>
+              <p className="cart-page-product-tags">
+                {item.tags.map((tag) => (
+                  <span key={item.id}>{tag} </span>
+                ))}
+              </p>
+              <p className="cart-page-product-sky">{item.sku}</p>
+            </div>
+
+            <div className="cart-page-product-amount">
+              <select
+                name="amount"
+                id="cart-page-product-amount"
+                value={item.quantity}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            <div className="cart-page-product-price-container">
+              <p className="cart-page-product-price">
+                ${item.price * item.quantity}
+              </p>
+            </div>
+
+            <div className="cart-page-product-button-container">
+              <button className="remove-btn">X</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
