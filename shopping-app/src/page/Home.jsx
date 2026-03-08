925aea788b798ed94f538ed07cfe28d811c5fc94
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-08 12:58:20 +0100

    Added btn change page container, btns that chage the page also added on click event for buttons

--- shopping-app/src/page/Home.jsx
+++ shopping-app/src/page/Home.jsx
@@ -1,13 +1,25 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShoppingContext } from "../context/ShoppingContext";
 import ProductCard from "../componants/ProductCard";
 
 function Home() {
-  const { data, loading, error, fetchProducts } = useContext(ShoppingContext);
+  const { data, loading, error, fetchProducts, page, setPage } =
+    useContext(ShoppingContext);
+
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  function handleClick() {
+    if (page === 1) {
+      setIsDisabled(true);
+    } else {
+      setIsDisabled(false);
+      setPage((prev) => prev - 1);
+    }
+  }
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [page]);
 
   if (!data) return null;
 
@@ -20,6 +32,22 @@ function Home() {
       {data.products.map((product) => (
         <ProductCard product={product} />
       ))}
+
+      <div className="btn-change-page-container">
+        <button
+          className="btn-change-page"
+          onClick={() => handleClick()}
+          disabled={isDisabled}
+        >
+          Previous
+        </button>
+        <button
+          className="btn-change-page"
+          onClick={() => setPage((prev) => prev + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
